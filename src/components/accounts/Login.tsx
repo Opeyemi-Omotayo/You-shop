932de0022fc49db50dbb4431b18f60e00dfeceaf
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-17 23:20:40 +0100

    designed the login page

--- src/components/accounts/Login.tsx
+++ src/components/accounts/Login.tsx
@@ -1,51 +1,53 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
- 
+
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const signIn = (e:any) => {
+    const signIn = (e: any) => {
         e.preventDefault();
 
-       
+
 
     }
 
-    const register = (e:any) => {
+    const register = (e: any) => {
         e.preventDefault();
 
-        
+
 
     }
 
     return (
-        <div > 
-            <Link to='/' style={{ textDecoration: "none" }}>
-                <div >
-                    <h2 >You Shop</h2>
+        <div >
+
+            <div className=' flex flex-col items-center justify-center ' >
+                <div className='w-4/6 md:w-3/6 lg:w-2/6'>
+                    <h1 className=' flex items-center justify-center text-4xl font-semibold m-8'>LOGIN</h1>
+
+                    <form>
+                        <h5>E-mail</h5>
+                        <input type='text' className='bg-gray-50 border border-gray-500 w-full p-2' value={email} onChange={e => setEmail(e.target.value)} />
+
+                        <h5>Password</h5>
+                        <input type='password' className='bg-gray-50 border border-gray-500 w-full p-2' value={password} onChange={e => setPassword(e.target.value)} />
+
+                        <div className='flex items-center justify-center'>
+                            <button type='submit' className=' text-white text-lg bg-orange-600 w-2/4 py-3 px-6 rounded-md shadow my-6' onClick={signIn}>SIGNIN</button>
+
+                        </div>
+                    </form>
+                    <div className='flex flex-col items-center justify-center'>
+                        <p className='text-sm'>
+                            By signing-in you agree to the You Shop Website Conditions of Use & Sale.
+                        </p>
+
+                        <button className='hover:text-orange-600 text-sm' onClick={register}>Create your You Shop Account</button>
+                    </div>
+
                 </div>
-            </Link>
 
-            <div >
-                <h1>Sign-in</h1>
-
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
-
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-
-                    <button type='submit'  onClick={signIn}>Sign In</button>
-                </form>
-
-                <p>
-                    By signing-in you agree to the eShop Website Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
-
-                <button  onClick={register}>Create your You Shop Account</button>
             </div>
         </div>
     )
